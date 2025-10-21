a7e61f4cb756e21a936dc206ab50683be63da885
yuniayoung86-jpg <yuniayoung86@gmail.com>
2025-10-21 13:48:36 +0700

    Create scrips.js

---
+++ scrips.js
@@ -0,0 +1,7 @@
+//1. pilih tombol berdasarkan 10-nya
+const tombolsapa =document.getelmenbyId('sapabutoon')
+// 2. tambahkan 'event listener" untuk aksi 'click'
+tombolsapa.addeventlistener('click',function() (
+  //3.jalankan kode ini ketika tombol di-klik
+                            alret('halo' terimakasih sudah berkunjung');
+  ));
